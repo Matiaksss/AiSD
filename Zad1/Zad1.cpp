df23a8a115076a1ebba56e635a5aac0015b69e2d
Matiaks <maciej.bogdan.kedziora@gmail.com>
2026-05-09 09:47:19 +0200

    vector based NeighbourMatrix.DFS()

--- Zad1/Zad1.cpp
+++ Zad1/Zad1.cpp
@@ -1,28 +1,26 @@
-﻿
-
-#include <iostream>
+﻿#include <iostream>
 #include <vector>
+enum colors {WHITE, GRAY, BLACK};
+bool DFS_flag;
 
 class NeighbourMatrix {
     int vertexes;
-    int edges;
-    int** matrix;
+    std::vector<std::vector<int>> matrix;
+    bool directed;
+
     void build_matrix() {
-        matrix = new int* [vertexes];
-        for (int i = 0; i < vertexes; i++) {
-            matrix[i] = new int[vertexes]();
-        }
+        matrix.resize(vertexes, std::vector<int>(vertexes, 0));
     }
 public:
-    NeighbourMatrix(int n, int m) : vertexes(n), edges(m) {
-        build_matrix();
+    NeighbourMatrix(int n, int m, bool directed = true) : vertexes(n), directed(directed) { build_matrix(); }
+    NeighbourMatrix(int n, bool directed = true) : vertexes(n), directed(directed) { build_matrix(); }
+    void add_incidence(int predecessor, int successor) {
+        predecessor--;
+        successor--;
+        matrix[predecessor][successor]++;
+        if (!directed)
+            matrix[successor][predecessor]++;
     }
-    ~NeighbourMatrix() {
-        for (int i = 0; i < vertexes; i++)
-            delete[] matrix[i];
-        delete[] matrix;
-    }
-    void add_incidence(int predecessor, int successor) { matrix[predecessor][successor] = 1; }
     void display() {
         for (int i = 0; i < vertexes; i++) {
             for (int j = 0; j < vertexes; j++) {
@@ -31,43 +29,81 @@ public:
             std::cout << '\n';
         }
     }
-
-    void display_as_AList() {
+    int get_vertexes_number() { return vertexes; }
+    int at(int n, int m) { return matrix[n][m]; }
+    void DFS() {
+        std::vector<colors> V(vertexes, WHITE);
+        DFS_flag = false;
+        DFS_visit(0, V);
+        for (int i = 0; i < vertexes; i++)
+            if (V[i] == WHITE)
+                DFS_flag = true;
+        std::cout << (DFS_flag ? "NO" : "YES");
+    }
+protected:
+    void DFS_visit(int v, std::vector<colors>& V, int parent = -1) {
+        V[v] = GRAY;
         for (int i = 0; i < vertexes; i++) {
+            if (matrix[v][i] && i != parent)
+                if (V[i] == WHITE && matrix[v][i] == 1) {
+                    DFS_visit(i, V, v);
+                    if (DFS_flag) break;
+                }
+                else {
+                    DFS_flag = true;
+                    break;
+                }
+
+        }
+        V[v] = BLACK;
+    }    //void display_as_AList() {
+    //    for (int i = 0; i < vertexes; i++) {
+    //        std::cout << i << ':';
+    //        for (int j = 0; j < vertexes; j++)
+    //            if (matrix[i][j])
+    //                std::cout << ' ' << j;
+    //        std::cout << '\n';
+    //    }
+    //}
+};
+
+class AdjacencyList {
+    std::vector<std::vector<int>> list;
+
+public:
+    AdjacencyList(NeighbourMatrix& matrix) {
+        list.resize(matrix.get_vertexes_number());
+        for (int i = 0; i < matrix.get_vertexes_number(); i++) {
+            for (int j = 0; j < matrix.get_vertexes_number(); j++) {
+                if (matrix.at(i,j))
+                    list.at(i).push_back(j);
+            }
+        }
+    }
+    void display() {
+        for (int i = 0; i < list.size(); i++) {
             std::cout << i << ':';
-            for (int j = 0; j < vertexes; j++)
-                if (matrix[i][j])
-                    std::cout << ' ' << j;
+            for (int j = 0; j < list.at(i).size(); j++)
+                std::cout << ' ' << list.at(i).at(j);
             std::cout << '\n';
         }
     }
 };
 
-class AdjacencyList {
-
-};
 
 
 int main()
 {
     int n, m;
     std::cin >> n >> m;
-    NeighbourMatrix matrix(n, m);
+    NeighbourMatrix matrix(n, m, false);
     while (std::cin >> n >> m) {
         matrix.add_incidence(n, m);
     }
-    matrix.display_as_AList();
-    std::cout << '\n';
+    //AdjacencyList Alist(matrix);
+    matrix.DFS();
+    //Alist.display();
+    //std::cout << '\n';
     matrix.display();
 }
 
-// Uruchomienie programu: Ctrl + F5 lub menu Debugowanie > Uruchom bez debugowania
-// Debugowanie programu: F5 lub menu Debugowanie > Rozpocznij debugowanie
-
-// Porady dotyczące rozpoczynania pracy:
-//   1. Użyj okna Eksploratora rozwiązań, aby dodać pliki i zarządzać nimi
-//   2. Użyj okna programu Team Explorer, aby nawiązać połączenie z kontrolą źródła
-//   3. Użyj okna Dane wyjściowe, aby sprawdzić dane wyjściowe kompilacji i inne komunikaty
-//   4. Użyj okna Lista błędów, aby zobaczyć błędy
-//   5. Wybierz pozycję Projekt > Dodaj nowy element, aby utworzyć nowe pliki kodu, lub wybierz pozycję Projekt > Dodaj istniejący element, aby dodać istniejące pliku kodu do projektu
-//   6. Aby w przyszłości ponownie otworzyć ten projekt, przejdź do pozycji Plik > Otwórz > Projekt i wybierz plik sln
